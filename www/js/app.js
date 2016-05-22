8276006234629a6de74fa856be8bdeb46dd552c4
Dana Joaquin <Dana Joaquin>
2016-05-22 11:43:39 -0400

    added login page

--- www/js/app.js
+++ www/js/app.js
@@ -31,8 +31,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+  // state for login page
+  //
+  .state('login', {
+	  url:'/login',
+	  templateUrl: 'templates/login.html',
+	  controller: 'LoginCtrl'
+  })
+  
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
@@ -80,6 +88,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
